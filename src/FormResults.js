ad30dc8e5cb16807c0402ebcc064e07aca77b720
Ryan Tonthat <rtonthat@asu.edu>
2019-11-06 20:12:51 -0700

    Final push for task 2

--- src/FormResults.js
+++ src/FormResults.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { ListItemSecondaryAction } from "@material-ui/core";
 export default function FormResults(props) {
-console.log(props);
+//console.log(props);
 var values = props.location.state;
 return <p> {values.firstName} {values.lastName} </p>;
 }
